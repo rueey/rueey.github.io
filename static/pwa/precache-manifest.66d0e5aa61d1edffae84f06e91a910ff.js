9f237a15724c269d12822d448c42e880c5954677
Rui Li <yuanruili2000@gmail.com>
2020-05-11 19:40:26 -0400

    deploy

---
+++ static/pwa/precache-manifest.66d0e5aa61d1edffae84f06e91a910ff.js
@@ -0,0 +1,40 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff2cad68ed8b7b61c1bff636169745dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c31765bb70b63dd2bc94a25ac42c5ff",
+    "url": "/resume.pdf"
+  },
+  {
+    "url": "/static/icons/icon_128x128.ee2678ee2de2d0955345a009430dae12.png"
+  },
+  {
+    "url": "/static/icons/icon_192x192.2e7a9f09eb4ce3a26b0d9ed7ea93e406.png"
+  },
+  {
+    "url": "/static/icons/icon_256x256.bfba6030220a6f9a2272a42de53917ea.png"
+  },
+  {
+    "url": "/static/icons/icon_384x384.60eff8c0760fa92894606fdc1fae7cf0.png"
+  },
+  {
+    "url": "/static/icons/icon_512x512.6475f27d549c0b523266292a0375d04e.png"
+  },
+  {
+    "url": "/static/icons/icon_96x96.111d9abf045a3e83af60c74a5da91e8c.png"
+  },
+  {
+    "url": "/static/images/e18977fd23c4850b464c07ab3d5e9f32.png"
+  },
+  {
+    "url": "/static/main.078b5382dfc117000383.css"
+  },
+  {
+    "url": "/static/main.078b5382dfc117000383.js"
+  },
+  {
+    "url": "/static/manifest.474d29df69a4e18182f683ca269d8696.json"
+  }
+]);
